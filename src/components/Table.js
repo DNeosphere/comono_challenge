eb2b08b13f436101143c5569f40059b48959a122
Dani Gomez <daniela.gomezp05@outlook.com>
2020-04-30 07:02:32 -0500

    [Feature] - CSS style modifications, adding more data

--- src/components/Table.js
+++ src/components/Table.js
@@ -15,9 +15,9 @@ class Table extends Component {
   getSortOrder(prop) {
     return function(a, b) {
         if (a[prop] > b[prop]) {
-            return -1;
-        } else if (a[prop] < b[prop]) {
             return 1;
+        } else if (a[prop] < b[prop]) {
+            return -1;
         }
         return 0;
     }
